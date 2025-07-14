1c740eaebdceb00b559102984df659bb1caf422f
Chriss5-2 <christiangiovannixd@gmail.com>
2025-07-14 16:14:21 -0500

    Ordenando la carpeta scripts

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -6,4 +6,10 @@ mkdir -p metrics
 
 kubectl top pods --all-namespaces --no-headers > "metrics/metric.csv"
 
-python scripts/plugin.py
+python src/plugin.py
+
+echo "Convirtiendo archivo json en csv"
+
+cd fixtures
+
+cat metrics.json | jq '.[] | join(",")' > metrics.csv
